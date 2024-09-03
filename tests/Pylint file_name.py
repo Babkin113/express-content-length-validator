bb1bd015619847e4f0b04397b14b2acea23070e2
Babkin113 <alexandr1181@yahoo.com>
2024-09-03 14:08:37 +0300

    devisces=[     {         ' device_tipe': 'cisco_ios':         'host: 192,168,1.1,',         'user nome': 'admin;'         ' password': 'password',         ' secret': 'secret':
        }
    {
        'device_tipe': ' cisco_ios'
        ' host: 192,168, 1.2',
        ' secret': ' secret',
    }

---
+++ tests/Pylint file_name.py
@@ -0,0 +1,22 @@
+from pickletools import pyint
+
+pyint "file_name".py
+with open ( output_file, mode="w",newline=") as file  # type: ignore
+writer=csv,writer ( file)
+writer.writorov ([' Devise': Interface; 'Status', Protocol])
+for device in devices:
+net_connect=ConnectHandler(**device)
+net _connet.enable()
+output=net_connet=net_connect.send_commond for line in output.splitlines()[1:]:(show ip int brief)
+if line:
+parts =line. split()
+writer.writerow[ device[' host'],parts [0],parts[4],
+net_connect disconnect()(parts[5])
+from netmiko import ConnectHanlder
+import datetime
+]
+
+
+
+
+
